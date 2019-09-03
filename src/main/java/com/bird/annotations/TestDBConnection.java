2db47040954813afef1f883ab0463911ce4ab10d
yzpbird <714770683@qq.com>
2019-09-03 21:27:16 +0800

    add test demo

---
+++ src/main/java/com/bird/annotations/TestDBConnection.java
@@ -0,0 +1,18 @@
+package com.bird.annotations;
+
+import org.testng.annotations.Test;
+
+public class TestDBConnection {
+
+    @Test
+    public void runOtherTest1(){
+        System.out.println("@Test--> runOtherTest1");
+    }
+
+    @Test
+    public void runOtherTest2(){
+        System.out.println("@Test--> runOtherTest2");
+    }
+
+
+}
